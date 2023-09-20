3505c9d6a703356eed80d2aa5ea4c3925209a1ee
Chrish303 <christhurajan333@gmail.com>
2023-09-20 16:59:11 +0530

    frontend

--- sep-19/login.js
+++ sep-19/login.js
@@ -1,21 +1,21 @@
-// let userdetails = [{
-//     email: "chrishbro71@gmail.com",
-//     password: "chrish123",
-//     name: "CHRISH"
-// },
-// {
-//     email: "barish71@gmail.com",
-//     password: "barish123",
-//     name: "BARISH"
-// },
-// {
-//     email: "azim@gmail.com",
-//     password: "azim123",
-//     name: "AZIM"
-// }
-// ]
-// let list = JSON.stringify(userdetails);
-// localStorage.setItem("user", list);
+let userdetails = [{
+    email: "chrishbro71@gmail.com",
+    password: "chrish123",
+    name: "CHRISH"
+},
+{
+    email: "barish71@gmail.com",
+    password: "barish123",
+    name: "BARISH"
+},
+{
+    email: "azim@gmail.com",
+    password: "azim123",
+    name: "AZIM"
+}
+]
+let list = JSON.stringify(userdetails);
+localStorage.setItem("user", list);
 function login() {
     alert("success")
 }
@@ -39,12 +39,14 @@ function logincheck() {
         }
 
 
+
     }
 
 }
 function logout() {
     document.getElementById("page").style.display = "block";
     document.getElementById("home").style.display = "none";
+    document.getElementById("table").style.display = "none"
 }
 function logged() {
 
@@ -53,6 +55,7 @@ function logged() {
         document.getElementById("name").innerHTML = `WELLCOME TO !${y}`;
         document.getElementById("page").style.display = "none";
         document.getElementById("home").style.display = "block";
+        document.getElementById("table").style.display = "none"
 
 
     }
@@ -60,7 +63,7 @@ function logged() {
 function sigin() {
     document.getElementById("reg").style.display = "block";
     document.getElementById("page").style.display = "none";
-
+    document.getElementById("table").style.display = "none"
 }
 
 function regis() {
@@ -91,5 +94,40 @@ function regis() {
     localStorage.setItem("user", JSON.stringify(pharsedata))
     document.getElementById("reg").style.display = "none";
     document.getElementById("page").style.display = "block";
-    
+    document.getElementById("table").style.display = "none"
 }
+function option() {
+    
+    // let hlist = ["a", "b", "c", "d"]
+    name_list=JSON.parse(localStorage.getItem("user"))
+    let htmldata = "";
+    for (i = 0; i < name_list.length; i++) {
+        n1=name_list[i].name
+        n2=name_list[i].email
+
+        htmldata = htmldata + `<tr>
+                                <td>${n1}</td>
+                                <td>${n2}</td>
+                                <td><button>Edit</button></td>
+                                <td><button onclick="Delete('${n2}')">Delete</button></td>
+                                </tr>`
+                               
+        document.getElementById("table").style.display = "block"
+    }
+    document.getElementById("list_body").innerHTML = htmldata;
+}
+function Delete(b){
+    // alert("s")
+    let details=JSON.parse(localStorage.getItem("user"))
+     let sdata=[]
+    for ( i=0;i<details.length;i++){
+       
+        if(details[i].email!=b){
+sdata.push(details[i])
+        }
+    }
+    localStorage.setItem("user",JSON.stringify(sdata))
+    option()
+}
+
+

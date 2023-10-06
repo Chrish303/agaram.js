05ce2563bfed53ddf0d3c2da398ecb01f948c101
Chrish303 <christhurajan333@gmail.com>
2023-10-06 17:09:44 +0530

    resumebuild

--- oct-5/practice/builder.js
+++ oct-5/practice/builder.js
@@ -14,9 +14,9 @@ function parseData(a,p_key) {
 
     else{
         resume_details[p_key]={}
-        resume_details[p_key][a.name]=a.value
+        
     }
-          
+    resume_details[p_key][a.name]=a.value 
     }
    
     else{
@@ -24,7 +24,7 @@ function parseData(a,p_key) {
 }
 show()
 }
-function skil(key,id){
+function skil(key,id,list){
     if(!resume_details[key]){
         resume_details[key]=[]
     }
@@ -33,15 +33,21 @@ resume_details[key].push(document.getElementById(id).value);
 document.getElementById(id).value=""
 let htmldata = "";
 for(i=0;i<resume_details[key].length;i++){
-    htmldata = htmldata +`<h5>${resume_details[key][i]}</h5> <button type="button" class="btn btn-primary" onclick="del(${'resume_details.key[i]'})" >
-    delete <span class="badge text-bg-secondary"></span> </button>`
+    htmldata = htmldata +`<div id="re" ><h5>${resume_details[key][i]}</h5> <button type="button" class="btn btn-primary" onclick="del('${[i]}','${[key]}')" >
+    delete <span class="badge text-bg-secondary"></span> </button></div>`
  
 
 }
-document.getElementById("lis").innerHTML=htmldata
-                               
+document.getElementById(list).innerHTML=htmldata                     
  show()
     }
+function del(f,g){
+    resume_details[g].splice(f,1)
+       let  dele=document.getElementById("re")
+       dele.remove()
+    show()
+        
+}
 function add(edu){
     edu_tem[edu.name]=edu.value
     console.log(edu_tem)
@@ -52,11 +58,10 @@ function up(d){
        
     }
     resume_details[d].push(edu_tem)
-    // let keys=Object.keys(edu_tem)
-    // for(i=0;i<keys;i++){
-    //  let each=data[i]
-    //  document.getElementById('each').value=""
-    // }
+    let keys=Object.keys(edu_tem)
+    for(i=0;i<keys.length;i++){
+     document.getElementById(keys[i]).value=""
+    }
 
     edu_tem={}
 
@@ -66,9 +71,3 @@ show()
 function show() {
     document.getElementById("display").innerHTML = JSON.stringify(resume_details, undefined, 2)
 }
-
-
-// const x=document.querySelector('#g,#h,#i,#j')
-//        x.forEach(input => {
-//          input.value="";
-//         });

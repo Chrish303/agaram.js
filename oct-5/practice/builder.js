80e9bde391a609b750ac99e3c33e6ddc601ac7b9
Chrish303 <christhurajan333@gmail.com>
2023-10-11 10:59:59 +0530

    resume builder

--- oct-5/practice/builder.js
+++ oct-5/practice/builder.js
@@ -1,73 +1,159 @@
-resume_details = {
-
-}
-let edu_tem={
-
-}
-function parseData(a,p_key) {
-   if (p_key){
-    if(resume_details[p_key]){
-        resume_details[p_key]={...resume_details[p_key]}
-    
+let resume_details={}
+let education_temp={}
+function parsedata(a,b){
+  if(b){
+    if(resume_details){
+      resume_details[b]={...resume_details[b]}
     }
-
-
     else{
-        resume_details[p_key]={}
-        
-    }
-    resume_details[p_key][a.name]=a.value 
-    }
-   
-    else{
-        resume_details[a.name]=a.value  
+      resume_details[b]={}
+         }
+    resume_details[b][a.name]=a.value
+ }
+ else{
+   resume_details[a.name]=a.value
+     }
+  display()
+   // resume_details[a.name]=a.value
+  // console.log(resume_details)
 }
-show()
-}
-function skil(key,id,list){
-    if(!resume_details[key]){
-        resume_details[key]=[]
-    }
-
-resume_details[key].push(document.getElementById(id).value);
+function skil(value,id,list){
+ if(!resume_details[value]){
+ resume_details[value]=[]
+ }
+resume_details[value].push(document.getElementById(id).value);
 document.getElementById(id).value=""
-let htmldata = "";
-for(i=0;i<resume_details[key].length;i++){
-    htmldata = htmldata +`<div id="re" ><h5>${resume_details[key][i]}</h5> <button type="button" class="btn btn-primary" onclick="del('${[i]}','${[key]}')" >
-    delete <span class="badge text-bg-secondary"></span> </button></div>`
- 
-
+let details= "";
+for(i=0;i<resume_details[value].length;i++){
+details = details +`<div id="create" ><h5>${resume_details[value][i]}</h5> <button type="button" class="btn btn-primary" onclick="del('${[i]}','${[value]}')" >delete <span class="badge text-bg-secondary"></span> </button></div>`
 }
-document.getElementById(list).innerHTML=htmldata                     
- show()
-    }
-function del(f,g){
-    resume_details[g].splice(f,1)
-       let  dele=document.getElementById("re")
-       dele.remove()
-    show()
-        
+document.getElementById(list).innerHTML=details 
+display()
 }
-function add(edu){
-    edu_tem[edu.name]=edu.value
-    // console.log(edu_tem)
-}
-function up(d){
-    if(!resume_details[d]){
-        resume_details[d]=[]
-       
-    }
-    resume_details[d].push(edu_tem)
-    let keys=Object.keys(edu_tem)
-    for(i=0;i<keys.length;i++){
-     document.getElementById(keys[i]).value=""
-    }
-
-    edu_tem={}
-
-show()
+function del(x,z){
+resume_details[z].splice(x,1)
+let  dele=document.getElementById("create")
+dele.remove()
+display()
 }
 
-function show() {
-    document.getElementById("display").innerHTML = JSON.stringify(resume_details, undefined, 2)
+
+function edu(status){
+  education_temp[status.name]=status.value
+  // console.log(education_temp)
 }
+
+
+function finish(data){
+ if(!resume_details[data]){
+ resume_details[data]=[]      
+ }
+resume_details[data].push(education_temp)
+education_temp={}
+let keys=Object.keys(education_temp)
+for(i=0;i<keys.length;i++){
+ document.getElementById(keys[i]).value=""
+ }
+display()
+}
+function display(){
+document.getElementById("show").innerHTML = JSON.stringify(resume_details, undefined, 2)
+}
+function create() {
+    $.ajax({
+        type: "post",
+        url: 'http://agaram.academy/api/action.php',
+        data: {request:"create_resume",
+        user:"chrish",
+        resume:{resume_details}
+    },
+        success: function (response) {
+        console.log("response",response)
+    },
+        error: function (fail) {
+            console.log("fail",fail)
+        }
+    });
+}
+function get() {
+    $.ajax({
+        type: "get",
+        url: "http://agaram.academy/api/action.php",
+        data: {
+            request: "get_user_resume",
+            user: "chrish",
+        },
+        success: function (response) {
+            console.log("response",JSON.parse (response))
+        },
+        error: function (erro) {
+            console.log("error", erro)
+        }
+    })
+}
+function remove(z) {
+    $.ajax({
+        type: "get",
+        url: "http://agaram.academy/api/action.php",
+        data: {
+            request: "delete_user_resume",
+            user: "chrish",
+            id:z
+        },
+        success: function (response) {
+            console.log("response",JSON.parse (response))
+        },
+        error: function (erro) {
+            console.log("error", erro)
+        }
+
+    })
+    get()
+}
+function table(){
+    window.location="build.html"
+}
+function show(){
+    $.ajax(
+        {
+            type: "get",
+            url: "http://agaram.academy/api/action.php",
+            data: {
+                request : "get_user_resume",
+                user : "chrish",
+            },
+            success: function (response) {
+                let res = JSON.parse(response)
+                let data=res.data;
+                let tabledata=""
+                for(i=0;i<data.length;i++){
+                    tabledata=tabledata+`<tr>
+                    <td>${data[i].id}</td>
+                    <td>${data[i].user}</td>
+                    <td><button type="button" class="btn btn-danger" onclick="remove('${data[i].id}')">Delete</button></td>
+                    </tr>`
+                }
+                $('#tbody').html(tabledata)
+            },
+            error: function (err) {
+                console.log('error', err)
+            }
+        }
+    )
+    
+}
+// function del(a) {
+//     $.ajax({
+//         type: "get",
+//         url: `https://jsonplaceholder.typicode.com/posts/`,
+//         data: '{}',
+//         success: function (response) {
+//             console.log(response)
+//         },
+//         error: function (fail) {
+//             console.log(fail)
+//         }
+//     });
+// }
+
+
---
+++ oct-5/practice/empty.js
@@ -0,0 +1,68 @@
+let resume_detail={}
+let temp={}
+function val(get_info,key_name){
+  if(key_name){
+    if(resume_detail[key_name]){
+      resume_detail[key_name]={...resume_detail[key_name]}
+    }else{
+      resume_detail[key_name]={}
+    }resume_detail[key_name][get_info.name]=get_info.value
+  }
+  else{
+    resume_detail[get_info.name]=get_info.value
+  }
+  display()
+}
+
+function add_item(key_name,id,tbl_id,edu_info){
+  if(edu_info){
+    if(!resume_detail[edu_info]){
+      resume_detail[edu_info]=[]
+    }
+      resume_detail[edu_info].push(temp)
+      let keyss=Object.keys(temp)
+      let final_out=""
+      for(i=0;i<keyss.length;i++){
+        document.getElementById(keyss[i]).value=""
+      }
+    temp={}
+    let ans=""
+    for(i=0;i<resume_detail[edu_info].length;i++){
+      let a=resume_detail[edu_info][i].institute_name
+      let b=resume_detail[edu_info][i].level
+      let c=resume_detail[edu_info][i].year
+      let d=resume_detail[edu_info][i].percentage
+      ans=ans+<tr id="${edu_info[i]}"><td>${a}</td><td>${b}</td><td>${c}</td><td>${d}</td><td><button type="button" class="btn btn-primary" onclick="del('${i}','${edu_info}')">remove</button></td></tr>
+    }document.getElementById("edu_tbl").innerHTML=ans
+    display()
+  }else{
+  if(key_name){
+    if(!resume_detail[key_name]){
+      resume_detail[key_name]=[]
+    }
+  }resume_detail[key_name].push(document.getElementById(id).value)
+  document.getElementById(id).value=""
+  let htmldata=""
+  for(i=0;i<resume_detail[key_name].length;i++){
+    htmldata=htmldata+<div id="${key_name[i]}" class="alert alert-danger"><button type="button" class="btn btn-primary" onclick="del('${[i]}','${key_name}')">remove</button><ol>${resume_detail[key_name][i]}</ol></div>
+  }document.getElementById(tbl_id).innerHTML=htmldata}
+  display()
+}
+
+function del(idx,key_name){
+  resume_detail[key_name].splice(idx,1)
+  result=document.getElementById(key_name[idx])
+  result.remove()
+  display()
+}
+
+function add_edu(got){
+  temp[got.name]=got.value
+}
+
+
+
+
+function display(){
+document.getElementById("show").innerHTML=JSON.stringify(resume_detail)
+}
